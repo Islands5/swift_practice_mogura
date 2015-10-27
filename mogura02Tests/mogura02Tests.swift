0dbbc5b2b203b10cd4b39b604103e0e4cc65ef95
g_ryotaro <goto@basicinc.jp>
2015-10-28 07:56:07 +0900

    Initial Commit

---
+++ mogura02Tests/mogura02Tests.swift
@@ -0,0 +1,36 @@
+//
+//  mogura02Tests.swift
+//  mogura02Tests
+//
+//  Created by 五島　僚太郎 on 2015/10/28.
+//  Copyright © 2015年 rg. All rights reserved.
+//
+
+import XCTest
+@testable import mogura02
+
+class mogura02Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
